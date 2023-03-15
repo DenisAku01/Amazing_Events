650f8370038f8c5d36beb947bff37634829e988f
Denis Manriquez <denis.manriquez@est.fi.uncoma.edu.ar>
2023-03-15 09:01:28 -0300

    molde de filtro cruzado

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,25 @@
 
 // muestra todas las card en general
-const divElement = document.getElementById("cardSection");
-  
+const divElement = document.getElementById("cardSection")
+const CONTENEDORCHECK = document.getElementById("checkSection")
+const INPUT = document.querySelectorAll("input")
 
+
+//      Llamadas de Funciones
+// Imprimir Cards
 printCard()
+//crea las Checkbox
+crearCheckBoxes(data.events)
+// Escuchar y Filtrar cruzado
+INPUT.addEventListener('input',filtradoPrincipal)
+// llamada al filtrado de categorias
+filtrarPorCategoria(data.events)
 
 
 
+
+
+//Imprime Todas las Cartas
 function printCard(){
   let card = '';
 
@@ -24,6 +37,53 @@ function printCard(){
   }
   divElement.innerHTML = card;
 }
-   
+// crea las checkbox necesarias por categoria
+function crearCheckBoxes(array){
+  let arrayCategories = array.map(data => data.events.category)
+  console.log(arrayCategories)
+  let setCategories = new Set(arrayCategories)
+  console.log(setCategories)
+  let arrayChecks = data.events.from(setCategories)
+  console.log(arrayChecks)
+  let checkboxes = ''
+  arrayChecks.forEach(category => {
+      checkboxes += `
+    <input type="checkbox" class="btn-check" id="${category}">
+    <label class="btn btn-outline-danger labelp" for="${category}">${category}</label>`
+  })
+  CONTENEDORCHECK.innerHTML = checkboxes
+}
 
+
+
+//Filtra todo incluyendo la categorias y el buscador y muestra por pantalla lo filtrado
+function filtradoPrincipal(){
+  let primerFiltro = filtrarPorTexto(people,INPUT.value)
+  let segundoFiltro = filtrarPorCategoria(primerFiltro)
+  pintarPersonas(segundoFiltro)
+}
+ 
+
+
+//Filtro el Buscador
+function filtrarPorTexto(array,texto){
+  let arrayFiltrado = array.filter(elemento => elemento.name.toLowerCase().includes(texto.toLowerCase()))
+  return arrayFiltrado
+}
   
+//filtrado por categoria
+function filtrarPorCategoria(array){
+  let checkboxes = document.querySelectorAll("input[type='checkbox']")
+  console.log(checkboxes);
+  let arrayChecks = Array.from(checkboxes)
+  let arrayChecksChecked = arrayChecks.filter(check => check.checked)
+  console.log(arrayChecksChecked);
+  let arrayChecksCheckedValues = arrayChecksChecked.map(checkChecked => checkChecked.value)
+  console.log(arrayChecksCheckedValues);
+  let arrayFiltrado = array.filter(elemento => arrayChecksCheckedValues.includes(elemento.country))
+  console.log(arrayFiltrado);
+  if(arrayChecksChecked.length > 0){
+      return arrayFiltrado
+  }
+  return array
+}
