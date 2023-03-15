769638706af177e604e7342d12045b97bea0f5a0
Denis Manriquez <denis.manriquez@est.fi.uncoma.edu.ar>
2023-03-15 13:13:47 -0300

    filtro Arreglado en Main he index

--- scripts/main.js
+++ scripts/main.js
@@ -1,89 +1,97 @@
-
 // muestra todas las card en general
 const divElement = document.getElementById("cardSection")
 const CONTENEDORCHECK = document.getElementById("checkSection")
-const INPUT = document.querySelectorAll("input")
+const INPUT = document.getElementById("search")
 
-
-//      Llamadas de Funciones
+// Llamadas de Funciones
 // Imprimir Cards
-printCard()
+printCard(data.events)
 //crea las Checkbox
 crearCheckBoxes(data.events)
 // Escuchar y Filtrar cruzado
 INPUT.addEventListener('input',filtradoPrincipal)
-// llamada al filtrado de categorias
-filtrarPorCategoria(data.events)
-
+CONTENEDORCHECK.addEventListener('change',filtradoPrincipal)
 
 
 
 
 //Imprime Todas las Cartas
-function printCard(){
+function printCard(array){
   let card = '';
-
-  for (let event of data.events) {
-    console.log(data.events);
-    card += `<div class="card cardp col-sm-6"  >
-                        <img src="${event.image}" class="bd-placeholder-img card-img-top svgp" alt="imagen del evento">
-                        <div class="card-body">
-                            <h5 class="card-title">${event.name}</h5>
-                            <p class="card-text">${event.description}</p>
-                            <a href="./detail.html" class="btn btn-danger">More Details</a>
-                            <p class="card-text">Price:${event.price}</p>
-                        </div>
-                 </div>`
+  
+  if(array.length > 0 ){
+    for (let event of array) {
+      card += `<div class="card cardp col-sm-6"  >
+                          <img src="${event.image}" class="bd-placeholder-img card-img-top svgp" alt="imagen del evento">
+                          <div class="card-body">
+                              <h5 class="card-title">${event.name}</h5>
+                              <p class="card-text">${event.description}</p>
+                              <a href="./detail.html" class="btn btn-danger">More Details</a>
+                              <p class="card-text">Price:${event.price}</p>
+                          </div>
+                   </div>`
+    }
+    divElement.innerHTML = card;
+  }else{
+    divElement.innerHTML = `<h2>NOT FOUND</h2>`
   }
-  divElement.innerHTML = card;
+
+
+
 }
 // crea las checkbox necesarias por categoria
 function crearCheckBoxes(array){
-  let arrayCategories = array.map(data => data.events.category)
+  let arrayCategories = array.map(data => data.category)
   console.log(arrayCategories)
   let setCategories = new Set(arrayCategories)
   console.log(setCategories)
-  let arrayChecks = data.events.from(setCategories)
+  let arrayChecks = Array.from(setCategories)
   console.log(arrayChecks)
   let checkboxes = ''
   arrayChecks.forEach(category => {
       checkboxes += `
-    <input type="checkbox" class="btn-check" id="${category}">
+    <input type="checkbox" class="btn-check" id="${category}" value="${category}">
     <label class="btn btn-outline-danger labelp" for="${category}">${category}</label>`
   })
   CONTENEDORCHECK.innerHTML = checkboxes
 }
 
-
-
 //Filtra todo incluyendo la categorias y el buscador y muestra por pantalla lo filtrado
 function filtradoPrincipal(){
-  let primerFiltro = filtrarPorTexto(people,INPUT.value)
+  
+  let primerFiltro = filtrarPorTexto(data.events, INPUT.value)
+  console.log(primerFiltro)
   let segundoFiltro = filtrarPorCategoria(primerFiltro)
-  pintarPersonas(segundoFiltro)
+  printCard(segundoFiltro)
 }
- 
 
 
 //Filtro el Buscador
 function filtrarPorTexto(array,texto){
   let arrayFiltrado = array.filter(elemento => elemento.name.toLowerCase().includes(texto.toLowerCase()))
+  console.log(arrayFiltrado)
   return arrayFiltrado
 }
-  
+ 
+
 //filtrado por categoria
 function filtrarPorCategoria(array){
   let checkboxes = document.querySelectorAll("input[type='checkbox']")
-  console.log(checkboxes);
+  // console.log(checkboxes); 
   let arrayChecks = Array.from(checkboxes)
   let arrayChecksChecked = arrayChecks.filter(check => check.checked)
-  console.log(arrayChecksChecked);
+  // console.log(arrayChecksChecked);
   let arrayChecksCheckedValues = arrayChecksChecked.map(checkChecked => checkChecked.value)
-  console.log(arrayChecksCheckedValues);
-  let arrayFiltrado = array.filter(elemento => arrayChecksCheckedValues.includes(elemento.country))
-  console.log(arrayFiltrado);
+  // console.log(arrayChecksCheckedValues);
+  let arrayFiltrado = array.filter(elemento => arrayChecksCheckedValues.includes(elemento.category))
+  console.log(arrayFiltrado)
+  console.log(array)
   if(arrayChecksChecked.length > 0){
       return arrayFiltrado
+  }else{
+    return array
   }
-  return array
+  
 }
+
+  
